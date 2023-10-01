2646619f5edf5d67d4a03d5de1ab105c4e93df1b
isabellexvr <isabellexavier59@gmail.com>
2023-10-01 02:47:45 -0300

    style: public key page

--- src/pages/Decryptation/index.jsx
+++ src/pages/Decryptation/index.jsx
@@ -1,32 +1,83 @@
 import styled from "styled-components";
 import colors from "../../assets/colors";
 import binary from "../../assets/binary-code.png";
+import {BsFillArrowLeftCircleFill} from "react-icons/bs"
+import { useNavigate } from "react-router-dom";
+import { FaRegCopy } from "react-icons/fa";
+import { StartButton } from "../styledComponents";
 
 export default function Decryptation() {
+  const navigate = useNavigate();
   return (
     <PageContainer>
       <Title>
-        <h1>Encriptação</h1>
+        <BsFillArrowLeftCircleFill onClick={() => navigate("/options")} />
+        <h1>
+          Desen<strong>cript</strong>ação
+        </h1>
       </Title>
       <RightContainer>
-        <img src={binary}/>
+        <img src={binary} />
+        <Form>
+          <div className="textarea">
+            <textarea
+              placeholder="Mensagem Encriptada" 
+              wrap="hard"
+              rows="4"
+              cols="40"
+            ></textarea>
+            <Line />
+          </div>
+          <div className="inputs">
+            <div className="input">
+              <SmallInput placeholder="p" />
+              <Line />
+            </div>
+            <div className="input">
+              <SmallInput placeholder="q" />
+              <Line />
+            </div>
+            <div className="input">
+              <SmallInput placeholder="e" />
+              <Line />
+            </div>
+          </div>
+          <EncryptedText>
+            <h1>cachorro</h1>
+            <FaRegCopy />
+          </EncryptedText>
+          <StartButton>DESENCRIPTAR</StartButton>
+        </Form>
       </RightContainer>
-
     </PageContainer>
   );
 }
 
 const PageContainer = styled.div`
-  background-color: red;
-  width: 100vw;
-  height: 100vh;
   display: flex;
+  width: 100vw;
+
+  display: flex;
+  align-items: center;
 `;
 
 const Title = styled.div`
   width: 30vw;
   height: 100vh;
   background-color: ${colors.darkPurple};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  >svg{
+    position: absolute;
+    top: 1vw;
+    left: 50%;
+    transform: translateX(-50%);
+    font-size: 2.2vw;
+    color: ${colors.lightPurple};
+    cursor: pointer;
+  }
   > h1 {
     color: #fff;
     text-align: center;
@@ -36,12 +87,98 @@ const Title = styled.div`
     font-weight: 700;
     line-height: normal;
     > strong {
-      color: #8d76b4;
+      color: ${colors.lightPurple};
     }
   }
 `;
 
 const RightContainer = styled.div`
-    background-color: ${colors.black};
+  background-color: ${colors.black};
+  width: 70vw;
+  position: relative;
+  > img {
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    opacity: 0.2;
+  }
+`;
 
-`
+const Form = styled.form`
+  height: 80vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  > .textarea {
+    font-family: Red Hat Mono;
+    position: relative;
+    > textarea {
+      all: unset;
+      width: 100%;
+      height: 100%;
+      color: white;
+      font-size: 1.6vw;
+      height: 7vw;
+      width: 35vw;
+    }
+  }
+  > .inputs {
+    display: flex;
+    justify-content: flex-start;
+    width: 35vw;
+
+    > .input {
+      font-family: Red Hat Mono;
+      position: relative;
+      margin-right: 3vw;
+    }
+  }
+`;
+
+const Line = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 0.15vw;
+  background-color: ${colors.mediumPurple};
+  bottom: 0;
+`;
+
+const SmallInput = styled.input`
+  color: white;
+  all: unset;
+  width: 50%;
+  height: 100%;
+  color: white;
+  font-size: 1.2vw;
+  height: 5vw;
+  width: 7vw;
+`;
+
+const EncryptedText = styled.div`
+  width: 35vw;
+  height: 10vw;
+  background-color: ${colors.white};
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  z-index: 1;
+  border-radius: 1.7vw;
+  border: 0.15vw solid ${colors.mediumPurple};
+  > h1 {
+    color: ${colors.black};
+    text-align: justify;
+    font-family: Red Hat Mono;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+  }
+  > svg {
+    font-size: 2.7vw;
+    cursor: pointer;
+    color: ${colors.darkPurple};
+  }
+`;
--- src/pages/PublicKey/index.jsx
+++ src/pages/PublicKey/index.jsx
@@ -1,10 +1,182 @@
 import styled from "styled-components";
+import { BiSolidKey } from "react-icons/bi";
+import colors from "../../assets/colors";
+import { FaRegCopy } from "react-icons/fa";
+import { StartButton } from "../styledComponents";
+import {BsFillArrowLeftCircleFill} from "react-icons/bs"
+import { useNavigate } from "react-router-dom";
 
-export default function PublicKey(){
-    return(
-        <>
+export default function PublicKey() {
+    const navigate = useNavigate();
+  return (
+    <PageContainer>
         
-        </>
-    )
+      <div className="top">
+      <BsFillArrowLeftCircleFill onClick={() => navigate("/options")} />
+        <BiSolidKey />
+        <h1>
+          Chave <strong>Pública</strong>
+        </h1>
+        <h3>
+          É necessário criar a chave pública antes de encriptar ou desencriptar.
+        </h3>
+      </div>
+
+      <Form>
+        <div className="inputs">
+          <div className="input">
+            <SmallInput placeholder="p" />
+            <Line />
+          </div>
+          <div className="input">
+            <SmallInput placeholder="q" />
+            <Line />
+          </div>
+          <div className="input">
+            <SmallInput placeholder="e" />
+            <Line />
+          </div>
+        </div>
+        <EncryptedText>
+          <h1>132, 7</h1>
+          <FaRegCopy />
+        </EncryptedText>
+        <StartButton>DESENCRIPTAR</StartButton>
+      </Form>
+    </PageContainer>
+  );
 }
 
+/* const Background = styled.div`
+    position: absolute;
+` */
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 60vw;
+  height: 100vh;
+  padding: 5vw;
+  box-sizing: border-box;
+  position: relative;
+  > .top {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    >svg:first-child{
+        position: absolute;
+        font-size: 2vw;
+        left: 12vw;
+        top: 5vw;
+        cursor: pointer;
+    }
+    > svg {
+      font-size: 5vw;
+      color: ${colors.mediumPurple};
+    }
+    > h1 {
+      color: #fff;
+      text-align: center;
+      font-family: Red Hat Text;
+      font-size: 50px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: normal;
+      > strong {
+        color: #8d76b4;
+      }
+    }
+    > h3 {
+      color: ${colors.grey};
+      text-align: center;
+      font-family: Red Hat Text;
+      font-size: 20px;
+      font-style: normal;
+      font-weight: 300;
+      line-height: normal;
+      width: 70%;
+    }
+  }
+`;
+
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  > .textarea {
+    font-family: Red Hat Mono;
+    position: relative;
+    > textarea {
+      all: unset;
+      width: 100%;
+      height: 100%;
+      color: white;
+      font-size: 1.6vw;
+      height: 7vw;
+      width: 35vw;
+    }
+  }
+  > .inputs {
+    display: flex;
+    justify-content: space-around;
+    width: 35vw;
+    margin-bottom: 4.5vw;
+    margin-top: 3vw;
+
+    > .input {
+      font-family: Red Hat Mono;
+      position: relative;
+      //margin-right: 3vw;
+    }
+  }
+`;
+
+const EncryptedText = styled.div`
+  width: 20vw;
+  height: 7vw;
+  background-color: ${colors.white};
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  z-index: 1;
+  border-radius: 1.7vw;
+  border: 0.15vw solid ${colors.mediumPurple};
+  margin-bottom: 4vw;
+  > h1 {
+    color: ${colors.black};
+    text-align: justify;
+    font-family: Red Hat Mono;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+  }
+  > svg {
+    font-size: 2vw;
+    cursor: pointer;
+    color: ${colors.darkPurple};
+  }
+`;
+
+const Line = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 0.15vw;
+  background-color: ${colors.mediumPurple};
+  bottom: 0;
+`;
+
+const SmallInput = styled.input`
+  color: white;
+  all: unset;
+  width: 50%;
+  height: 100%;
+  color: white;
+  font-size: 1.2vw;
+  height: 5vw;
+  width: 7vw;
+`;
