679236857bc3737de435988bc644cf5de6a263e8
Isabelle Xavier <isabellexavier59@gmail.com>
2023-11-23 19:48:27 -0300

    decryptation url

--- src/pages/Decryptation/index.jsx
+++ src/pages/Decryptation/index.jsx
@@ -41,7 +41,7 @@ export default function Decryptation({
       //https://rsa-back.onrender.com
       //http://localhost:4000/desencriptar
       axios
-        .post("https://rsa-back.onrender.com/desencriptar", form)
+        .post("https://thoughtful-tan-culottes.cyclic.app/desencriptar", form)
         .then((answer) => {
           setDecryptedText(answer.data);
           toast.success("Mensagem Desencriptada com Sucesso!", {
